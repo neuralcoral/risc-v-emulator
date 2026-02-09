39b9d1932670fdeceeac928e9c1ed4df7aba1ec1
🧠 neuralcoral 🪸 <george@neuralcoral.io>
2026-02-08 20:19:38 -0800

    [WIP] Started UART

---
+++ src/devices/uart.cc
@@ -0,0 +1,9 @@
+#include "uart.h"
+
+riscv::byte_t UART::read(const riscv::mem_addr_t &addr) const {
+    return 0;
+}
+
+void UART::write(const riscv::mem_addr_t &addr, const riscv::byte_t &value) {
+
+}
